b9b1395f215900815a98e30067f8d131e584d32b
burkozel <71974079+burkozel@users.noreply.github.com>
2021-09-12 20:04:58 +0300

    Delete infons.py

--- infons.py
+++
@@ -1,3 +0,0 @@
-group_token = '0271ad11a02b4d29c2b5dc65c46b767e597dfff8daea5b01e62d243dbf5559a54a1127334137cd12c6751'
-user_token = '56577c1d668cc3937555d7a9ea72006222f20d9c1eaa098147f244bfc8721acbad535a6b41c1e4dc66a58'
-bd_path = 'postgresql://postgres:admin@localhost:5432/vkinder'
